98f0bff457cb59f7cf8834c529c412c89dfb9117
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-06-05 15:22:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,17 +3,19 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Constraint=Matter.Constraint;
+const Constraint= Matter.Constraint;
 
 let engine;
 let world;
 
-var ground;
 var rope;
+var ground;
+
 
 var ball;
 
 var btn1;
+
 
 function setup() {
   createCanvas(400,400);
@@ -21,30 +23,42 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   
-  
-
-  ground =new Ground(200,390,400,20);
-
-
-  var ball_options = {
+   var ball_options = {
     restitution: 0.95,
   }
    
+  ground =new Ground(200,390,400,20);
+
 
   ball = Bodies.circle(100,200,20,ball_options);
   World.add(world,ball);
-  
-  var options={
-    //a point and PE body
-    pointA:{x:200,y:20}, //pointA,pointB
-    bodyB:ball,         //bodyA,bodyB
-    stiffness:0.1,
-    length:100
 
+    //BodyA or BodyB
+    //PointA or PointB
+    //Length
+    //stiffness
+
+  //PointA(x,y) and BodyB(ball) -use
+  var options={
+    pointA:{x:200,y:20}, 
+    bodyB:ball, 
+    length:100,
+    stiffness:0.1
   }
-  
-  rope=Constraint.create(options);
+   
+  rope=Matter.Constraint.create(options);
   World.add(world,rope)
+
+  // rope=Matter.Constraint.create({
+  //   pointA:{x:200,y:20}, 
+  //   bodyB:ball, 
+  //   length:100,
+  //   stiffness:0.1
+  // });
+  // World.add(world,rope)
+  
+  
+  
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -56,22 +70,21 @@ function draw()
   background(51);
   Engine.update(engine);
   
+  ellipse(ball.position.x,ball.position.y,20);
+  ground.show();
+  
+  //Draw the constraint
+  //line(x1,y1,x2,y2)
+  push()
+
+  stroke("green");
+  strokeWeight(5);
+  line(rope.pointA.x,rope.pointA.y,ball.position.x,ball.position.y)
+
+  pop()
   
 
-  ellipse(ball.position.x,ball.position.y,20,20);
-  ground.show();
-
-  //point-x,y to balls x,y
-  push();
-  stroke("red");
-  strokeWeight(5);
-  line(rope.pointA.x,rope.pointA.y,ball.position.x,ball.position.y);
-  pop();
-  Engine.update(engine);
 }
 
-
-
-  
 
 
